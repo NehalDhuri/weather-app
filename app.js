af9ee1a85ba0c57cda6c6712a6314f2c3770d211
Nehal Dhuri <nehaldhuri221@gmail.com>
2021-12-17 17:09:59 +0530

    dynamic weather card completed

---
+++ app.js
@@ -0,0 +1,50 @@
+var cityName = document.querySelector('#search-bar');
+var searchIcon = document.querySelector('.search-icon');
+const apiKey = "2206bc7a96424fcf526f8259a873e585";
+var temp = document.querySelector(".temp-value");
+var minTemp = document.querySelector(".min-temp");
+var description = document.querySelector(".value .title");
+var pressure = document.querySelector(".presValue");
+var humidity = document.querySelector(".humValue");
+var visibility = document.querySelector(".visibValue");
+
+
+var serverURL = 'https://api.openweathermap.org/data/2.5/weather?q=sawantwadi&appid=2206bc7a96424fcf526f8259a873e585&units=metric';
+
+function getURL(name) {
+    return serverURL;
+}
+
+function errorHandler(error){
+    alert("something wrong with server try again after some time");
+}
+
+function clickEventHandler() {
+    var name = cityName.value;
+    
+    fetch(getURL(name))
+        .then(response => response.json())
+        .then(data => {
+            var tempValue = data['main']['temp'];
+            var presValue = data['main']['pressure'];
+            var humValue = data['main']['humidity'];
+            var visibValue = data['visibility'];
+            var minTempValue = data['main']['temp_min'];
+            var descValue = data['weather'][0]['description'];
+
+            temp.innerHTML = parseInt(tempValue) ;
+            minTemp.innerHTML = parseInt(minTempValue) ;
+            description.innerHTML = descValue;
+            pressure.innerHTML = presValue;
+            humidity.innerHTML = humValue;
+            visibility.innerHTML = visibValue/1000;
+            console.log(data);
+        })
+
+        .catch(errorHandler)
+
+    
+}
+
+searchIcon.addEventListener("click", clickEventHandler);
+
